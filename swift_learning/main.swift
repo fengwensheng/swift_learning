cc4ff816ca58677cb295135a18269a9c02ed98d5
vincent_feng_2020 <vincent_feng_2020@qq.com>
2020-11-18 09:44:25 +0800

    initial commit

--- swift_learning/main.swift
+++ swift_learning/main.swift
@@ -7,5 +7,55 @@
 
 import Foundation
 
-print("Hello, World!")
+print("foo")
+
+//1 everything is object, no primitive type
+let s = 1.description
+print(s)
+
+//2 judge object 'extends'
+
+//3 extendsion
+extension Int {
+    func sayHello() {
+        print("Hello, I'm \(self)")
+    }
+}
+5.sayHello()
+
+//4 object
+//4.1 class
+class Dog {
+    func bark()  {
+        print("woof")
+    }
+}
+//4.2 struct, eg. Int
+struct Moe {
+}
+//4.3 enum
+enum Jack {
+}
+
+//5 declaration, initialization
+let one = 1
+var two = 2
+two = one
+//let is Constant
+//one = two
+//swift is also a strong type language
+//two = "two"
+print(two)
+
+//6 function
+func go() {
+    let one = 1
+    var two = 2
+    two = 1
+}
+
+
+
+
+
 
---
+++ swift_learning/test.swift
@@ -0,0 +1,17 @@
+//
+//  test.swift
+//  swift_learning
+//
+//  Created by fws on 2020/11/17.
+//
+
+import Foundation
+
+class Manny {
+    let name = "manny"
+//    print(name)
+    func sayName() {
+        print(name)
+    }
+}
+
